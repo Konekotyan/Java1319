be111392177a22b42936ea09016a23b384958d00
Konekotyan <Pistolero01@mail.ru>
2020-09-24 01:14:07 +0300

    shape

---
+++ src/main/Shape/STest.java
@@ -0,0 +1,36 @@
+package main.Shape;
+
+public class STest {
+    public static void main(String[] args) {
+        Shape s1 = new Circle("black", false, 12);
+        System.out.println(s1);
+        System.out.println(s1.getArea());
+        System.out.println(s1.getPerimeter());
+
+        Circle c1 = (Circle) s1;
+        System.out.println(c1);
+        System.out.println(c1.getArea());
+        System.out.println(c1.getPerimeter());
+        System.out.println(c1.getRadius());
+
+        Shape s3 = new Rectangle("green", false, 3.0, 6.0);
+        System.out.println(s3);
+        System.out.println(s3.getArea());
+        System.out.println(s3.getPerimeter());
+        System.out.println(s3.getColor());
+
+        Rectangle r1 = (Rectangle) s3;
+        System.out.println(r1);
+        System.out.println(r1.getArea());
+        System.out.println(r1.getLength());
+
+        Shape s4 = new Square(10);
+        System.out.println(s4);
+        System.out.println(s4.getArea());
+
+        Rectangle r2 = (Rectangle) s4;
+        System.out.println(r2);
+        System.out.println(r2.getArea());
+        System.out.println(r2.getLength());
+    }
+}
